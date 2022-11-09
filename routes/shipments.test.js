f76c56112e24704ca0b1956631d8beeb40c8ba25
clchen-arcadia <clchen.arcadia@gmail.com>
2022-11-08 20:31:09 -0500

    Add comments following code review. Exercise finished up until further studies

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,11 +1,31 @@
 "use strict";
 
+// let {shipProduct} = require("../shipItApi");
+// shipProduct = jest.fn();
+
+// TODO: Why can't I do it like above??
+// --> well, we def can't do it like this...
+
+// to be put at top of test fn
+// shipProduct.mockReturnValue(42)
+
+const shipItApi = require("../shipItApi"); // <--- working ver
+shipItApi.shipProduct = jest.fn(); // <--- working ver
+
 const request = require("supertest");
 const app = require("../app");
 
-
 describe("POST /", function () {
   test("valid", async function () {
+
+    //mock shipProduct
+    shipItApi.shipProduct.mockReturnValue(42); // <---working ver
+    // shipProduct.mockReturnValue(42)
+
+
+    // per test preferable -- more granular
+    // often you would .mockValueOnce() !
+
     const resp = await request(app).post("/shipments").send({
       productId: 1000,
       name: "Test Tester",
@@ -13,10 +33,15 @@ describe("POST /", function () {
       zipcode: "12345-6789",
     });
 
-    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+    expect(resp.body).toEqual({ shipped: 42 }); // <--- we hard-coded 42
+    // we COULD be MORE specific above. it will DEFINITELY be 42
   });
 
   test("throws error if empty request body", async function () {
+
+    //mock shipProduct
+    // shipItApi.shipProduct.mockReturnValue(42);
+
     const resp = await request(app)
       .post("/shipments")
       .send();
@@ -24,6 +49,10 @@ describe("POST /", function () {
   });
 
   test("returns JSON error if valid input is not provided", async function () {
+
+    //mock shipProduct
+    // shipItApi.shipProduct.mockReturnValue(42);
+
     const response = await request(app)
       .post("/shipments")
       .send({
@@ -34,10 +63,13 @@ describe("POST /", function () {
       });
     expect(response.statusCode).toEqual(400);
     expect(response.body.error.message.length).toEqual(4);
-    expect(response.body.error.message.join("")).toContain("productId","name","zipcode","addr");
-      // comment to see which field has incorrect formatting
-      // check length of error, see if error msg includes field keyword
-      // make 2 right, 2 wrong inputs, check that they are 2 length in msg
-      //
+    expect(response.body.error.message.join(""))
+      .toContain("productId", "name", "zipcode", "addr");
+    // test to exactly the thing.
+
+    // comment to see which field has incorrect formatting
+    // check length of error, see if error msg includes field keyword
+    // make 2 right, 2 wrong inputs, check that they are 2 length in msg
+    //
   })
 });
--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,11 +1,24 @@
 "use strict";
 
+const AxiosMockAdapter = require("axios-mock-adapter");
+const axios = require("axios");
+const axiosMock = new AxiosMockAdapter(axios);
+
+const BASE_URL = "http://localhost:3001";
+
+
+
 const {
   shipProduct,
 } = require("./shipItApi");
 
 
 test("shipProduct", async function () {
+  // Mock axios requests
+  axiosMock.onPost(`${BASE_URL}/ship`)
+  .reply(200, { receipt: {shipId: 42} });
+
+
   const shipId = await shipProduct({
     productId: 1000,
     name: "Test Tester",
@@ -13,5 +26,5 @@ test("shipProduct", async function () {
     zip: "12345-6789",
   });
 
-  expect(shipId).toEqual(expect.any(Number));
+  expect(shipId).toEqual(42);
 });
